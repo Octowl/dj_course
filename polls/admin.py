6863959edd549241e56cacb00684b51adbb0316f
Octowl <alsaff1987@gmail.com>
2015-06-02 11:59:56 +0300

    Customize admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -2,16 +2,30 @@ from django.contrib import admin
 from .models import Poll, Choice, Response
 
 
-class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
+
+
+class ResponseInline(admin.TabularInline):
+    model = Response
+    raw_id_fields = ('choice',)
+
 
 class PollAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'question', 'choice_count')
-    inlines = [ChoiceInline, ]
+    list_filter = ('category',)
+    search_fields = ['name', 'question']
+    inlines = [ChoiceInline, ResponseInline]
+
 
 class ResponseAdmin(admin.ModelAdmin):
-    list_display = ('poll_name', 'choice_label', 'comment')
+    list_display = ('poll_name', 'choice', 'comment')
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('poll_name', 'label')
+    raw_id_fields = ('poll',)
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Response, ResponseAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,8 +2,27 @@ from django.db import models
 
 
 class Poll(models.Model):
+
+    DEMOGRAPHICS = 'DEM'
+    POLITICS = 'POL'
+    SPORTS = 'SPT'
+    TECHNOLOGY = 'TEC'
+    ACADEMICS = 'ACD'
+    TRAVEL = 'TRL'
+
+    CATEGORY_CHOICES = (
+        ('DEM', 'Demographics'),
+        ('POL', 'Politics'),
+        ('SPT', 'Sports'),
+        ('TEC', 'Technology'),
+        ('ACD', 'Academics'),
+        ('TRL', 'Travel'),
+    )
+
     name = models.CharField("poll name", max_length=64)
-    category = models.CharField("poll category", max_length=64)
+    category = models.CharField("poll category",
+                                choices=CATEGORY_CHOICES,
+                                max_length=64)
     question = models.TextField(blank=True)
 
     def __str__(self):
@@ -22,14 +41,15 @@ class Choice(models.Model):
     def __str__(self):
         return "{}".format(self.label)
 
+    def poll_name(self):
+        return self.poll.name
+
 
 class Response(models.Model):
+    poll = models.ForeignKey(Poll, verbose_name="poll question")
     choice = models.ForeignKey(Choice, null=True, blank=True)
     comment = models.TextField(blank=True)
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def poll_name(self):
         return self.choice.poll.name
-
-    def choice_label(self):
-        return self.choice.label
